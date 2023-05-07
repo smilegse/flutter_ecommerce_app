472e697f3477fe36721d8a2ad25b444047e4bfe1
Md. Abu Bakar Siddique <abms.siddique@gmail.com>
2023-05-07 14:54:18 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+// Analysis: Details - target - deadline -
+// Structure - Layer -Feature -Hexa
+// State management - Getx
+// 1. UI + Functional 2. Statement management 3. Api integration
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'ui/screens/splash_screen.dart';
+
+void main() {
+  runApp(const CraftyBay());
+}
+
+class CraftyBay extends StatelessWidget {
+  const CraftyBay({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
---
+++ lib/ui/utils/app_colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF07ADAE);
+const Color greyColor = Color(0xFF3C3F41);
